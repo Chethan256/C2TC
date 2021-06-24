0db418da90d39468b0e27e2b90cb86f9810ff03e
User <User@DESKTOP-H773ET5>
2021-06-24 19:18:43 +0530

    project

---
+++ ex2/src/ex2/Example.java
@@ -0,0 +1,16 @@
+package ex2;
+
+public class Example {
+	   public static void main(String args[]){
+	      int num=70;
+	      if( num < 100 ){
+		  /* This println statement will only execute,
+		   * if the above condition is true
+		   */
+		  System.out.println("number is less than 100");
+	      }
+	      else {
+	    	  System.out.println("number is greater than 100");
+	      }
+	   }
+	}
